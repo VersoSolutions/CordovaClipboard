fbf9427cac1527d521470ad6d9d66b7ddb2767ff
Fernando Garcia <Korreca@users.noreply.github.com>
2016-05-09 16:29:03 +0200

    Update ClipboardProxy.js

--- src/windows/ClipboardProxy.js
+++ src/windows/ClipboardProxy.js
@@ -27,23 +27,40 @@ var cordova = require('cordova');
 module.exports = {
 
     copy:function(options) {
-       var text = "";
+        var text = "";
+        try {
+            text = JSON.parse(options)[0];
+        }
+        catch (e) {
+            //DispatchCommandResult(new PluginResult(PluginResult.Status.JSON_EXCEPTION));
+            return;
+        }
        
-       var dataPackage = new Windows.ApplicationModel.DataTransfer.DataPackage();
-       dataPackage.setText(text);
-       Windows.ApplicationModel.DataTransfer.Clipboard.setContent(dataPackage);
-       
+        try {
+            var dataPackage = new Windows.ApplicationModel.DataTransfer.DataPackage();
+            dataPackage.setText(text);
+            Windows.ApplicationModel.DataTransfer.Clipboard.setContent(dataPackage);
+            //DispatchCommandResult(new PluginResult(PluginResult.Status.OK, text));
+        }
+        catch (e) {
+            //DispatchCommandResult(new PluginResult(PluginResult.Status.ERROR));
+        }
     },
     paste:function(options) {
       var text = "";
       
-      var dataPackageView = Windows.ApplicationModel.DataTransfer.Clipboard.getContent();
-      if (dataPackageView.contains(Windows.ApplicationModel.DataTransfer.StandardDataFormats.text)) {
-          dataPackageView.getTextAsync().then(function (value) {
+      try {
+        var dataPackageView = Windows.ApplicationModel.DataTransfer.Clipboard.getContent();
+        if (dataPackageView.contains(Windows.ApplicationModel.DataTransfer.StandardDataFormats.text)) {
+            dataPackageView.getTextAsync().then(function (value) {
             text = value;
+            //DispatchCommandResult(new PluginResult(PluginResult.Status.OK, text));
           });
+        }
       }
-      
+      catch (e) {
+            //DispatchCommandResult(new PluginResult(PluginResult.Status.ERROR));
+        }
     }
 }; // exports
 
